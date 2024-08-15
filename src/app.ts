620c4844d2493cece6ca572bd98abe7fdf53eb23
Gusttavo Ruy <7leckler@gmail.com>
2024-08-15 15:40:04 -0300

    feat: add scroll-snap in carrossel

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,25 @@
-const Form = document.querySelector('form')!;
+const sectionCards = document.querySelector('.sectionCard')!;
+const doorLeft = document.querySelector('.doorLeft')!;
+const doorRight = document.querySelector('.doorRight')!;
+const carrossel = document.querySelector('.carrossel');
 
-Form.addEventListener('submit', (e) => {
-  e.preventDefault();
+const observerFirst = new IntersectionObserver(([entry]) => {
+  if (entry.isIntersecting) {
+    console.log(entry.target);
+  }
+}, {
+  root: carrossel,
+  rootMargin: '300px',
 });
+
+const observerLast = new IntersectionObserver(([entry]) => {
+  if (entry.isIntersecting) {
+    sectionCards.appendChild(sectionCards.firstElementChild!);
+  }
+}, {
+  root: carrossel,
+  rootMargin: '300px',
+});
+
+observerFirst.observe(doorLeft);
+observerLast.observe(doorRight);
